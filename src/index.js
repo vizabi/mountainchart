960d1854b6e45a4968dc5f1bd61bcd29fb821873
Angie the Fairytale <konzeptmeister@gmail.com>
2018-06-01 18:09:08 +0200

    Add UI model to treemenu

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const MountainChart = Vizabi.Tool.extend("MountainChart", {
     }, {
       component: Vizabi.Component.get("treemenu"),
       placeholder: ".vzb-tool-treemenu",
-      model: ["state.marker", "state.time", "locale"]
+      model: ["state.marker", "state.time", "locale", "ui"]
     }, {
       component: Vizabi.Component.get("datawarning"),
       placeholder: ".vzb-tool-datawarning",
