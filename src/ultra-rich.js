8ce30ddd667ca43ab970f70a60a0d36bd9087238
angie <konzeptmeister@gmail.com>
2022-07-02 18:16:56 +0200

    Refactor Billy logic for better performance, use datasource.drilldowns

--- src/ultra-rich.js
+++ src/ultra-rich.js
@@ -20,9 +20,12 @@ class MCUltraRich extends BaseComponent {
 
     this.ultrarichMarkerName = options.ultrarichMarkerName;
     this.ultrarichEncName = options.ultrarichEncName;
-    this.catalog = null;
-    this.catalogReady = false;
-    this._getCatalog();
+    this.drilldowns = null;
+    this.billy = new Set();
+    this.drilldownsReady = false;
+    this.billyReady = false;
+    this.colorMap = {};
+    this.colorMapReady = false;
   }
 
 
@@ -30,70 +33,92 @@ class MCUltraRich extends BaseComponent {
     return {
       frame: this.model.encoding.frame,
       color: this.model.encoding.color,
-      ultrarichMarker: this.root.model.markers[this.ultrarichMarkerName]
+      ultrarichMarker: this.root.model.markers[this.ultrarichMarkerName],
+      ultrarichFrame: this.root.model.markers[this.ultrarichMarkerName].encoding.frame
     };
   }
 
   draw() {
     this.localise = this.services.locale.auto(this.MDL.frame.interval);
+    this.addReaction(this.copyframevalue);
+    this.addReaction(this.getDrillDowns);
+    this.addReaction(this.getBillies);
+    this.addReaction(this.getColorMapping);
     this.addReaction(this.redraw);
-    //this.addReaction(this._getCatalog);
-    this.addReaction(this._copyframevalue);
-  }
-
-  _isModelReady() {
-    return this.MDL.ultrarichMarker.state == Utils.STATUS.READY && this.catalogReady;
   }
 
 
-  _getCatalog() {
-    // this.model.data.spaceCatalog.then(spaceCatalog => {
-    //     console.log(spaceCatalog)
-    // })
-    const setMembershipFlags = this.model.data.source.availability.data
-      .map(m => m.value)
-      .filter(f => f.includes("is--"))
-    const countryprops = this.model.data.source.availability.data
-      .filter(f => f.key.includes("country") && setMembershipFlags.includes("is--" + f.value))
-      .map(m => m.value)
+  getDrillDowns() {
 
-    this.model.data.source.query({
-      select: {
-        key: ["geo"],
-        value: countryprops
-      },
-      from: "entities"
-    }).then(data => {
-      this.catalog = data.forKey(["geo"]);
-      this.catalogReady = true
+    const DRILLDOWN_CONCEPT = "country";
+    const dim = this.model.data.space[0];
+    const entity = this.parent.atomicSliceData.map(m => m[dim]);
+    
+    if(this.drilldownsReady == entity.join("")) return;
+    this.drilldownsReady = false;
+    this.model.data.source.drilldown({dim, entity})
+      .then( catalog => {
+        this.drilldowns = catalog[DRILLDOWN_CONCEPT];
+        this.drilldownsReady = entity.join("");
+      }); 
+
+  }
+
+  copyframevalue() {
+    this.MDL.ultrarichFrame.config.value = this.MDL.frame.value;
+  }
+
+  getBillies() {
+    if(this.MDL.ultrarichMarker.state !== Utils.STATUS.READY || !this.drilldownsReady) return;
+
+    this.billyReady = false;
+    const allBilly = this.MDL.ultrarichMarker.encoding.geos.data.domainData;
+    for(let [_, billy] of allBilly) {
+      if(d3.intersection(this.drilldowns, billy.countries.split(";")).size) 
+        this.billy.add(billy.person);
+      else
+        this.billy.delete(billy.person);
+    }
+    this.billyReady = true;
+  }
+
+  getColorMapping(){
+    const dim = "geo";
+    const entitySet = "country";
+
+    this.colorMapReady = false;
+    this.model.data.source.drillupCatalog.then(c => {
+      const entities = c[dim][entitySet].get(Symbol.for("drill_up"));
+      for(let [key, entity] of entities) {
+        this.colorMap[key] = entity[this.MDL.color.data.concept];
+      }
+      this.colorMapReady = true;
     });
-  }
-  _copyframevalue() {
-    this.MDL.ultrarichMarker.encoding.frame.config.value =
-      this.MDL.frame.value;
+    
   }
 
-  _getBillyData(mountains) {
-    return this.MDL.ultrarichMarker.dataArray
-        .filter(f => 
-          d3.intersection(mountains.map(m => m.geo), Object.values(this.catalog.get(f.geo))).size
-          && this.MDL.frame.value - f.time == 0)
-        .slice(0, 10)
-  }
 
   redraw(options = {}) {
     const _this = this;
 
-    if (!this._isModelReady()) return;
+    if(this.MDL.ultrarichMarker.state !== Utils.STATUS.READY || !this.drilldownsReady || !this.billyReady || !this.colorMapReady) return;
 
     const r = 4;
     const hash = (string) => {
-      return d3.sum(string.split("").map(m=>m.charCodeAt(0)) ) % this.parent.profileConstants.minHeight 
+      return d3.sum(string.split("").map(m=>m.charCodeAt(0)) )  
     }
 
+    const getBillyData = () => { 
+      if (this.MDL.ultrarichFrame.scale.domain[0] - this.MDL.frame.value > 0 || this.MDL.frame.value - this.MDL.ultrarichFrame.scale.domain[1] > 0)
+        return [];
+      else
+        return this.MDL.ultrarichMarker.dataArray
+          .filter(f => this.billy.has(f.person))
+          .slice(0, 10)
+    }
 
     let circles = this.DOM.container.selectAll("circle")
-      .data(this._getBillyData(this.parent.atomicSliceData), d => d.person);
+      .data(getBillyData(), d => d.person);
 
     circles.join("circle")
       .style("stroke", "black")
@@ -109,9 +134,11 @@ class MCUltraRich extends BaseComponent {
         //   context.DOM.xAxis.call(context.xAxis.highlightValue("none"));
         // })
       })
-      .style("fill", d => this.parent.MDL.color.scale.d3Scale(this.catalog.get(d.geo)[this.MDL.color.data.concept]))
+      .style("fill", d => 
+        this.parent.MDL.color.scale.d3Scale(this.colorMap[d.geos.split(";")[0]])
+      )
       .attr("r", r)
-      .attr("cy", d => this.parent.yScale(0) - hash(d.person) - r)
+      .attr("cy", d => this.parent.yScale(0) - hash(d.person) % this.parent.profileConstants.minHeight - r)
       .each(function(d){
         const view = d3.select(this);
 
@@ -132,6 +159,8 @@ class MCUltraRich extends BaseComponent {
 
 const decorated = decorate(MCUltraRich, {
   "MDL": computed,
-  "catalogReady": observable
+  "drilldownsReady": observable,
+  "billyReady": observable,
+  "colorMapReady": observable,
 });
 export { decorated as MCUltraRich };
